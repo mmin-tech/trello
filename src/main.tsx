71b02ebf609074feb88d5cb57760a29d2f413a4c
min <min20071996@gmail.com>
2024-01-19 11:18:42 +0700

    setup mui and eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+import theme from './theme'
+import App from './App'
+import { DevSupport } from '@react-buddy/ide-toolbox'
+import { ComponentPreviews, useInitial } from './dev'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <DevSupport ComponentPreviews={ComponentPreviews}
+        useInitialHook={useInitial}
+      >
+        <App/>
+      </DevSupport>
+    </ThemeProvider>
+  </React.StrictMode>
 )
